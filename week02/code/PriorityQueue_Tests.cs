abb0896f24007f7c7a7572b3f9a83699156092d4
Charles Ofuasia <ofu23001@byui.edu>
2024-11-15 18:26:30 +0100

    all tests passed and documented

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,35 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    // Scenario: Create a PriorityQueue and add persons with equal priority to the queue
+    // Expected Result: The person at index [0] is first dequeued
+    // Defect(s) Found: The equality sign in the >= caused the higest priority to move to the next person in the loop
+    // even though all persons have equal priority. Removed the = from comparison after which the test passed.
+    public void TestPriorityQueue_EqualPrioriries()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("John", 1);
+        priorityQueue.Enqueue("Jane", 1);
+        priorityQueue.Enqueue("Doe", 1);
+        var result = priorityQueue.Dequeue();
+        Assert.AreEqual("John", result);
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    // Scenario: create PriorityQueue and add persons with different priorities, with the highest Priority not at index[0]
+    // Expected Result: Person with highest Priority is dequeued even though not at index[0]
+    // Defect(s) Found: Not all items are inspected in the loop because the loop is less than than priorityQueue.Count
+    // passed after corrections were made
+    public void TestPriorityQueue_VaryingPriorities()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("John", 1);
+        priorityQueue.Enqueue("Doe", 2);
+        priorityQueue.Enqueue("Jane", 9);
+        var result = priorityQueue.Dequeue();
+        Assert.AreEqual("Jane", result);
     }
 
-    // Add more test cases as needed below.
+
+
 }
--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -11,7 +11,8 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
     // run until the queue is empty
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: 1 New persons were inserted to the front of the queue instead of being pushed to the back
+    // I changed the _queue.Insert to _queue.Add in the Enqueue method of PersonQueue class.
     public void TestTakingTurnsQueue_FiniteRepetition()
     {
         var bob = new Person("Bob", 2);
@@ -43,7 +44,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3)
     // After running 5 times, add George with 3 turns.  Run until the queue is empty.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
-    // Defect(s) Found: 
+    // Defect(s) Found: No defects was found. Test passed.
     public void TestTakingTurnsQueue_AddPlayerMidway()
     {
         var bob = new Person("Bob", 2);
@@ -85,7 +86,8 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: The GetnextPerson  method failed to take into account of case where a person has turns of 0 or less
+    // which should result to infinite number of turns. Test passed after a conditional statement was added to correct this.
     public void TestTakingTurnsQueue_ForeverZero()
     {
         var timTurns = 0;
@@ -116,7 +118,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: No defect because I have already modified the GetNextPerson method to handle Turns <= 0 in the last test.
     public void TestTakingTurnsQueue_ForeverNegative()
     {
         var timTurns = -3;
@@ -143,7 +145,7 @@ public class TakingTurnsQueueTests
     [TestMethod]
     // Scenario: Try to get the next person from an empty queue
     // Expected Result: Exception should be thrown with appropriate error message.
-    // Defect(s) Found: 
+    // Defect(s) Found: No defect. Test passed.
     public void TestTakingTurnsQueue_Empty()
     {
         var players = new TakingTurnsQueue();
