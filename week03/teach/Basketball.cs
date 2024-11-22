705c6495953f778775345b88bd076c1f5e76f0f9
Charles Ofuasia <ofu23001@byui.edu>
2024-11-22 11:01:18 +0100

    Team activitiy completed

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -23,14 +23,29 @@ public class Basketball
         reader.TextFieldType = FieldType.Delimited;
         reader.SetDelimiters(",");
         reader.ReadFields(); // ignore header row
-        while (!reader.EndOfData) {
+        while (!reader.EndOfData)
+        {
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+
+            if (!players.ContainsKey(playerId))
+            {
+                players.Add(playerId, points);
+            }
+            else
+            {
+                players[playerId] += points;
+            }
         }
 
-        Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
 
-        var topPlayers = new string[10];
+        // Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
+
+        var topPlayers = players.OrderByDescending(p => p.Value).ToArray();
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"{topPlayers[i]}");
+        }
     }
 }
